4faae9e66010d2e76bb685286645096041a21923
brandonroberts58 <51340741+brandonroberts58@users.noreply.github.com>
2019-08-16 13:58:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,53 @@ function myFunction() {
   }
 }
 
+var myGallery = [{
+    title: "Image 1",
+    description: "Description 1",
+    src: "1.jpg"
+},{
+    title: "Image 2",
+    description: "Description 2",
+    src: "2.jpg"
+},{
+    title: "Image 3",
+    description: "Description 3",
+    src: "3.jpg"
+}];
+
+Spotlight.show(myGallery);
+
+Spotlight.show(myGallery,{
+  animation: "slide",
+  control: ["autofit", "zoom"],
+  autohide: true,
+  fullscreen: true,
+  zoom: true,
+  zoomin: true,
+  zoomout: true,
+  reset: true,
+  theme: true,
+  player: true,
+  infinite: true,
+  theme: "white",
+  page: true,
+  preloader: true,
+  prefetch: true
+});
+Spotlight.close();
+Spotlight.next();
+Spotlight.prev();
+Spotlight.goto(3);
+Spotlight.zoom(1.5);
+Spotlight.theme();
+Spotlight.theme("white");
+Spotlight.theme("dark");
+Spotlight.fullscreen();
+Spotlight.fullscreen(true);
+Spotlight.fullscreen(false);
+Spotlight.autofit();
+Spotlight.autofit(true);
+Spotlight.autofit(false);
+Spotlight.menu();
+Spotlight.menu(true);
+Spotlight.menu(false);
